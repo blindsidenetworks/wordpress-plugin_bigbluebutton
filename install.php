9d3f5cba69fe1c6baa089771eee5d0ddab988186
jfederico <jesus@123it.ca>
2017-09-08 19:56:55 +0000

    v2.0.0: Remove rooms on uninstall

--- install.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (!defined('WP_INSTALL_PLUGIN')) {
-    exit();
-}
-
-error_log("Installing...");
-// Install code goes here
--- uninstall.php
+++ uninstall.php
@@ -8,12 +8,31 @@ error_log("Uninstalling");
 // Uninstall code goes here
 global $wpdb;
 
-$tables = array('bigbluebutton', 'bigbluebutton_log');
+// Remove old tables if exist
+$tables = array('bigbluebutton', 'bigbluebutton_logs');
 foreach ($tables as $table) {
     $sql = "DROP TABLE IF EXISTS " . $wpdb->prefix . $table;
     $wpdb->query($sql);
 }
 
+// Remove old options if exist
+delete_option('bigbluebutton_url');
+delete_option('bigbluebutton_salt');
+
+// Remove rooms
+$args = array (
+    'post_type' => 'bbb-room',
+    'nopaging' => true
+);
+$query = new WP_Query($args);
+while ($query->have_posts()) {
+    $query->the_post();
+    $id = get_the_ID();
+    wp_delete_post($id, true);
+}
+wp_reset_postdata();
+
+// Remove options
 delete_option('bigbluebutton_endpoint');
 delete_option('bigbluebutton_secret');
 
