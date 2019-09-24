dc3a261138580977d3149837d5676861de11bd75
Amy <amydu.1998@outlook.com>
2019-09-24 14:52:31 -0400

    fix spacing

--- public/partials/bigbluebutton-optional-recordings-display.php
+++ public/partials/bigbluebutton-optional-recordings-display.php
@@ -3,7 +3,7 @@
         <button id="bbb-recordings-display"><?php esc_html_e('Hide'); ?></button>
     </div>
     <div id="recordings-list">
-        <?php if(empty($recordings)) { ?>
+        <?php if (empty($recordings)) { ?>
             <?php esc_html_e('This room does not yet have any recordings.', 'bigbluebutton'); ?>
         <?php } else { ?>
             <div class="table-container" role="table" aria-label="Destinations">
@@ -12,10 +12,10 @@
                 <div class="flex-row" role="columnheader"><?php esc_html_e('Date'); ?></div>
                 <div class="flex-row" role="columnheader"><?php esc_html_e('Link'); ?></div>
             </div>
-            <?php foreach($recordings as $recording) { ?>
+            <?php foreach ($recordings as $recording) { ?>
                 <div class="flex-table row" role="rowgroup">
                     <div class="flex-row first" role="cell"><?php echo urldecode($recording->name); ?></div>
-                    <div class="flex-row" role="cell"><?php echo date_i18n($date_format, (int)$recording->startTime / 1000); ?></div>
+                    <div class="flex-row" role="cell"><?php echo date_i18n($date_format, (int) $recording->startTime / 1000); ?></div>
                     <div class="flex-row" role="cell"><a href="<?php echo $recording->playback->format->url; ?>"><?php esc_html_e('View', 'bigbluebutton'); ?></a></div>
                 </div>
             <?php } ?>
