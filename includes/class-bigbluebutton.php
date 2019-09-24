66e62c5247445c40f98fb63f974deebd24b5aa23
Amy <amydu.1998@outlook.com>
2019-09-24 15:46:33 -0400

    WP-2: admin add update bbb url and secret (#18)
    * add settings to rooms admin panel
    
    * update file descriptions in admin class
    
    * remove error log

--- includes/class-bigbluebutton.php
+++ includes/class-bigbluebutton.php
@@ -156,6 +156,7 @@ class Bigbluebutton {
 
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
+		$this->loader->add_action( 'admin_menu', $plugin_admin, 'room_server_settings' );
 
 	}
 
