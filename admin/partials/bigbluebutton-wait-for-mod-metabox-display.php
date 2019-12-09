073fb56428f87208566f93342fe6f5471f7e6567
Amy <amydu.1998@outlook.com>
2019-12-09 16:46:36 -0500

    allow user to edit room properties

--- admin/partials/bigbluebutton-wait-for-mod-metabox-display.php
+++ admin/partials/bigbluebutton-wait-for-mod-metabox-display.php
@@ -1,6 +1,5 @@
-<?php if ( $existing_value != '' ) { ?>
-	<label><?php esc_html_e( 'Wait for Moderator', 'bigbluebutton' ); ?>: <?php esc_html_e( $existing_value, 'bigbluebutton' ); ?></label>
-<?php } else { ?>
-	<label><?php esc_html_e( 'Wait for Moderator', 'bigbluebutton' ); ?>: </label>
-	<input name="bbb-room-wait-for-moderator" type="checkbox" value="checked">
-<?php } ?>
+<label><?php esc_html_e( 'Wait for Moderator', 'bigbluebutton' ); ?>: </label>
+<input name="bbb-room-wait-for-moderator" type="checkbox" value="checked"
+<?php if ( 'true' === $existing_value) { ?>
+	checked
+<?php } ?>>
